11d078bed4aa5b79fec5435fb71870d630d845d6
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2020-07-17 13:01:36 +0530

    API Defined

---
+++ example1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	link "linkparser"
+	"os"
+	"fmt"
+)
+
+func main() {
+	filename := flag.String("file", "ex1.html", "File that contains the HTML to be parsed")
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	links, err := link.Parser(f)
+	fmt.Println(links)
+
+}
---
+++ parser.go
@@ -0,0 +1,12 @@
+package linkparser
+
+import "io"
+
+type Link struct {
+	Href string
+	Text string
+}
+
+func Parser(r io.Reader) ([]Link, error) {
+	return nil, nil
+}
