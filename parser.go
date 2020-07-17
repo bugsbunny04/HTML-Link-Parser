9c57107650473920254bd316b833210d7b4d6e62
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2020-07-17 13:21:06 +0530

    DFS

--- parser.go
+++ parser.go
@@ -1,6 +1,11 @@
 package linkparser
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"golang.org/x/net/html"
+)
+
 
 type Link struct {
 	Href string
@@ -8,5 +13,23 @@ type Link struct {
 }
 
 func Parser(r io.Reader) ([]Link, error) {
-	return nil, nil
+	doc, err := html.Parse(r)
+	if err!=nil {
+		panic(err)
+	}
+	dfs(doc,"")
+	return nil,nil
 }
+
+func dfs(n *html.Node,padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<"+msg+">"
+	}
+	fmt.Println(padding,msg)
+	for c:=n.FirstChild;c!=nil;c=c.NextSibling {
+		dfs(c,padding+" ")
+	}
+}
+
+
